666f9a4251a83f760139c2fe44ef76e6b56630cf
realhesam <hesamehsanimoghaddam2004@gmail.com>
2025-07-21 19:33:22 +0430

    added home page,login & signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.views import generic
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
+
+
