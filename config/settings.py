c177cffa01bebbdb6a48cbdc3e1938bd5bd572ba
realhesam <hesamehsanimoghaddam2004@gmail.com>
2025-07-25 20:47:39 +0430

    add static & navbar

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -145,3 +144,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # Email Config
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# static files config
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static')),]
