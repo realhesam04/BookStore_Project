d1abca7d99501b6bfc53d79673627a3f8e37473a
realhesam <hesamehsanimoghaddam2004@gmail.com>
2025-07-28 19:24:06 +0430

    added pictures for book covers

--- config/urls.py
+++ config/urls.py
@@ -25,5 +25,8 @@ urlpatterns = [
     path('',include('pages.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
     path('accounts/',include('accounts.urls')),
-    path('book/',include('books.urls')),
-] + static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
+    path('books/',include('books.urls')),
+]
+# + static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
