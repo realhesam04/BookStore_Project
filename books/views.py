c8beebb6cb116b8ddedcdd8583791411207edfe6
realhesam <hesamehsanimoghaddam2004@gmail.com>
2025-07-29 18:48:27 +0430

    added some features to Comment section

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
+from .forms import CommentForm
 
 from .models import Book
 
@@ -20,7 +21,22 @@ def book_detail_view(request, pk):
     book = get_object_or_404(Book, pk=pk)
     book_comments = book.comments.all()
 
-    return render(request, 'book/book_detail.html', {'book': book, 'comments': book_comments})
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.book = book
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'book/book_detail.html', {
+        'book': book,
+        'comments': book_comments,
+        'comment_form': comment_form
+    })
 
 
 class BookCreateView(generic.CreateView):
